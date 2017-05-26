c034bec2ec6af64706b8fa22624405b4400a667d
TOQUET Armand <armand@bi-kay.com>
2017-05-26 20:31:08 +0200

    messages admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-    	return view('admin.dashboard');
+        $messages=DB::table('messages')->get();
+    	return view('admin.dashboard')->with(['messages'=>$messages]);
     }
 
     public function experience()
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.admin')
 
 @section('content')
+<table class="table table-bordered table-striped table-hover table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Mail
+			</th>
+			<th>
+				Subject
+			</th>
+			<th>
+				Message
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($messages as $message)
+		<tr>
+			<td>
+				{{$message->mail}}
+			</td>
+			<td>
+				{{$message->subject}}
+			</td>
+			<td>
+				{{$message->message}}
+			</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+
+
+
 
 @endsection
